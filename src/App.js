814fbe6d8a518a702e9e33394feb94038cc32a14
iskanderbellil <iskanderbellil1997@gmail.com>
2022-06-13 18:23:44 +0200

    sec

--- src/App.js
+++ src/App.js
@@ -3,25 +3,25 @@ import Profile from './Profile/Profile';
 import image from './Profile/image.jpg';
 
 
+import React from 'react'
 
-
-
-function App() {
+function App () {
   const name="Bellil Iskander"
   const bio="loading"
   const profession="student at GoMyCode"
 
-const handleName=()=> {
- alert(name)
-  
- }
+  const handleName=(str)=> {
+    alert(`My name is ${str}`);
+  }
+    
 
   return (
     <div>
-      <Profile myName={name} aboutMe={bio} myProfession={profession} handleName={handleName}/>  
-       <img style={{width:200,height:200,top:22, left:20}} src={image} alt='my photo'></img>  
-    </div>
-);
+    <Profile name={name} bio={bio} profession={profession} handleName={handleName}/>  
+     <img style={{width:200,height:200,top:22, left:20}} src={image} alt='my photo' />
+  </div>
+  )
 }
+
 
 export default App;
--- src/Profile/Profile.js
+++ src/Profile/Profile.js
@@ -4,33 +4,34 @@ import PropTypes from 'prop-types';
 
 const Profile = (props) => {
   console.log(props)
+
+  const handleClick = () => props.handleName(props.name)
   return (
   
   <div>
   
-  <h1 style={{color:"#18204B" }}>My Name is {props.myName} </h1>
-  <h3 style={{backgroundColor:"#18204B" , color:"white", fontSize:30 }}> I am {props.aboutMe} and {props.myProfession} </h3>
+  <h1 style={{color:"#18204B" }}>My Name is {props.name} </h1>
+  <h3 style={{backgroundColor:"#18204B" , color:"white", fontSize:30 }}> I am {props.bio} and {props.profession} </h3>
   <p>{props.children}</p>
-  <button onClick={props.handleName}>Who am I?</button>
+  <button onClick={handleClick}>Who am I?</button>
   
   </div>
   )
 };
 
 Profile.defaultProps={
-  myName:"What's your name",
-  aboutMe:"what's your bio",
-  myProfession:" what's your profession",
-  children:"my img",
+  name:"What's your name",
+  bio:"what's your bio",
+  myProfession:" what's your profession"
+
 }
 
 
 
 Profile.propTypes={
-  myName: PropTypes.string,
-  aboutMe: PropTypes.string,
-  myProfession: PropTypes.string,
-  children:PropTypes.element,
+  name: PropTypes.string,
+  bio: PropTypes.string,
+  profession: PropTypes.string,
   handleName:PropTypes.func 
 }
 
